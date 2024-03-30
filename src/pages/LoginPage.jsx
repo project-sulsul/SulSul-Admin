3f0c18f358eb9a86a6890938ce41229b0a8851aa
tmlee <tmlee@pluszero.co.kr>
2024-03-30 17:55:49 +0900

    fix: Fix syntax error

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -30,9 +30,9 @@ export default function LoginPage() {
 
   return (
     <Box sx={{ display: "flex", height: "100vh", justifyContent: "center", alignItems: 'center' }}>
-      <Paper elevation={5} sx={{p: 3, widRefth: "50%"}}>
+      <Paper elevation={5} sx={{p: 3, width: "50%"}}>
         <h1>SULSUL Admin</h1>
-        
+
         <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
           <TextField required inputRef={idRef} label="idRef" variant="outlined" />
           <TextField required type="password" inputRef={pwRef} label="PW" variant="outlined" />
